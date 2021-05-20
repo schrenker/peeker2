087708521772244b3890a606e48f5105954a3615
schrenker <zawadzkis95@gmail.com>
2021-05-20 19:33:16 +0200

    most of GetHosts done

--- host/host.go
+++ host/host.go
@@ -12,9 +12,7 @@ type Host struct {
 	Hostname string
 	IP       string
 	Port     string
-
-	Cfg *ssh.ClientConfig
-
+	Cfg      *ssh.ClientConfig
 	Services []string
 
 	Cmd   string
@@ -58,10 +56,29 @@ func parseYAMLConfig() *yamlConfig {
 	return &cfg
 }
 
-// func prepareSSHConfig(user, keyPath string) (*ssh.ClientConfig, error) { return nil, nil }
+func prepareSSHConfig(user, keyPath string) (*ssh.ClientConfig, error) { return nil, nil }
 
 // func parseHost(services []string) {}
 
-// func GetHosts() ([]*Host, error) {
-// 	yaml := parseYAMLConfig()
-// }
+func GetHosts() []*Host {
+	yamlFile := parseYAMLConfig()
+
+	ret := make([]*Host, len(yamlFile.yamlHosts))
+
+	for i := range yamlFile.yamlHosts {
+		sshcfg, err := prepareSSHConfig(yamlFile.yamlHosts[i].user, yamlFile.yamlHosts[i].keyPath)
+		if err != nil {
+			log.Fatalln(err)
+		}
+
+		ret[i] = &Host{
+			Hostname: yamlFile.yamlHosts[i].hostname,
+			IP:       yamlFile.yamlHosts[i].ip,
+			Port:     yamlFile.yamlHosts[i].port,
+			Services: yamlFile.yamlHosts[i].services,
+			Cfg:      sshcfg,
+		}
+	}
+
+	return ret
+}
