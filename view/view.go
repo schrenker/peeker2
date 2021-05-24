fe910c107b864ec05b78d21bd424f10eaa28cd29
schrenker <zawadzkis95@gmail.com>
2021-05-25 00:02:39 +0200

    generate view done

--- view/view.go
+++ view/view.go
@@ -10,8 +10,8 @@ import (
 
 func generateBanner(disks, services config.Index) []string {
 	banner := make([]string, 3+len(disks)+len(services))
-	banner[0], banner[1], banner[2] = "1m", "5m", "15m"
-	ind := 3
+	banner[0] = "load"
+	ind := 1
 	for i := 0; i < len(disks); i++ {
 		banner[ind] = disks[i]
 		ind++
@@ -23,7 +23,21 @@ func generateBanner(disks, services config.Index) []string {
 	return banner
 }
 
-func generateView(hosts []*host.Host, banner []string) [][]string { return nil }
+func generateView(hosts []*host.Host, banner []string) [][]string {
+	ret := make([][]string, len(hosts)+1)
+
+	ret[0] = banner
+
+	for i := range hosts {
+		tmp := make([]string, len(banner))
+		for j := range banner {
+			tmp[j] = hosts[i].State[banner[j]]
+		}
+		ret[i+1] = tmp
+	}
+
+	return ret
+}
 
 func display(view [][]string) {
 	tm.Clear()
