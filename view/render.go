0d4663358ffb399dab3511987015451b0221ec23
schrenker <zawadzkis95@gmail.com>
2021-05-25 23:10:08 +0200

    split render from view file

---
+++ view/render.go
@@ -0,0 +1,35 @@
+package view
+
+import (
+	"time"
+
+	"github.com/schrenker/peeker2/config"
+	"github.com/schrenker/peeker2/host"
+)
+
+func generateBanner(disks, services config.Index) []string {
+	banner := make([]string, 3+len(disks)+len(services))
+	banner[0] = "load"
+	ind := 1
+	for i := 0; i < len(disks); i++ {
+		banner[ind] = disks[i]
+		ind++
+	}
+	for i := 0; i < len(services); i++ {
+		banner[ind] = services[i]
+		ind++
+	}
+	return banner
+}
+
+func Render(hosts []*host.Host, globalCfg config.GlobalConfig) {
+	host.UpdateStatusAll(hosts, globalCfg.DiskIndex, globalCfg.ServiceIndex)
+	banner := generateBanner(globalCfg.DiskIndex, globalCfg.ServiceIndex)
+
+	for {
+		view := newView(hosts, banner)
+		view.display()
+		time.Sleep(time.Duration(globalCfg.Interval) * time.Second)
+	}
+
+}
